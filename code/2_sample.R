b97f87b1e505d70909661ba666c39ec22f3f970e
ballardtj <ballardtj@gmail.com>
2018-12-12 15:19:20 +1000

    set up model where v_false is fixed to 1 and covariates predict v_true

--- code/2_sample.R
+++ code/2_sample.R
@@ -51,14 +51,14 @@ stan_list$COVS = stan_list$COVS[,c(1:6,9)]
 stan_list$Nvars = stan_list$Nvars-3
 
 #sample
-fit=stan(file="models/lba_vtfB_reciprocal.stan",
+fit=stan(file="models/lba_vtfB_reciprocal_vf_fixed.stan",
          data=stan_list,
          pars=c('v_true','v_false','B'),
          include=F,
          cores=4,
          #init_r = 1,
          control=list(max_treedepth=20),
-         #iter=10,
+         iter=10,
          chains=4
 )
 
